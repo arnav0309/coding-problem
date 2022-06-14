6ef2c81f2273fc5e1be0062f3495c6ed333ac7a6
arnav0309 <201952204@iiitvadodara.ac.in>
2022-06-14 19:52:15 +0530

    14-jun-2022

--- DP/mcm.cpp
+++ DP/mcm.cpp
@@ -44,15 +44,39 @@ long long int mcm(vi &v,int i,int j){
     }
     return ans;
 }
-int dp[100][100];
-ll mcmDp(vi &v,int i,int j){
-    if(i==j) return 0;
-   if(dp[i][j]!=-1) return dp[i][j];
-   ll ans = INT_MAX;
-    for(int k=i;k<j;k++){
-        ans = min(ans,mcmDp(v,i,k)+mcmDp(v,k+1,j)+v[i-1]*v[k]*v[j]);
+// int dp[100][100];
+// ll mcmDp(vi &v,int i,int j){
+//     if(i==j) return 0;
+//    if(dp[i][j]!=-1) return dp[i][j];
+//    ll ans = INT_MAX;
+//     for(int k=i;k<j;k++){
+//         ans = min(ans,mcmDp(v,i,k)+mcmDp(v,k+1,j)+v[i-1]*v[k]*v[j]);
+//     }
+//     return dp[i][j] = ans;
+// }
+
+ll mcmBu(vector<ll>&v){
+    int n = v.size();
+    vector<vector<ll>>dp(n,vector<ll>(n,INT_MAX));
+    for(int i=0;i<n;i++) dp[i][i]=0;
+
+    for(int len=2;len<n;len++){
+        for(int i=1;i<=(n-len);i++){
+            int j = i+len-1;
+            //if(j==n) continue;
+            //dp[i][j] = INT_MAX;
+            for(int k=i;k<j;k++){
+                dp[i][j] = std::min(dp[i][j],dp[i][k]+dp[k+1][j]+v[i-1]*v[k]*v[j]);
+            }
+        }
     }
-    return dp[i][j] = ans;
+    loop(i,0,n-1){
+        loop(j,0,n-1){
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return dp[1][n-1];
 }
 
 int main(int argc, char const *argv[])
@@ -63,12 +87,13 @@ int main(int argc, char const *argv[])
    int n;
    cin>>n;
    vi v(n);
-   memset(dp,-1,sizeof(dp));
+   //memset(dp,-1,sizeof(dp));
    for(int i=0;i<n;i++){
        cin>>v[i];
    }
-  cout<<mcm(v,1,n-1)<<endl;
-  cout<<mcmDp(v,1,n-1)<<endl;
+  //cout<<mcm(v,1,n-1)<<endl;
+  //cout<<mcmDp(v,1,n-1)<<endl;
+  cout<<mcmBu(v)<<endl;
     return 0;
 
 }
